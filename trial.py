9b02777addae25379281c3f459ee6046eb9817c2
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-05-11 01:36:48 +0300

    Learned Data Types

---
+++ trial.py
@@ -0,0 +1,21 @@
+#1. string
+animals="cows, cats"
+
+print(animals)
+print(f"We have many {animals} and dogs")
+#2. intergers-----whole numbers
+#3. floats---decimal numbers
+#4. lists----arrays
+students=["David", "Denis", "Davis", "Dave"]
+students[1]
+students.pop(3)
+
+#5. turples
+schools=("Moringa", "Kabete")
+print(schools)
+#6. sets
+hotels={"Serena", "La Joam"}
+print(hotels)
+#7. dictionaries
+#8. booleans
+#9. None
